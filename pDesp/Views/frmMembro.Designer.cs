060fa7c11daf24e9baf3bd48313d7d4b6510cdf5
Fernando Mariano Pereira <fernandovmp@outlook.com>
2019-11-10 20:02:44 -0300

    Exibe os membros salvos no banco de dados

---
+++ pDesp/Views/frmMembro.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace pDesp.Views
+{
+    partial class frmMembro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMembro));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.paginaDados = new System.Windows.Forms.TabPage();
+            this.dgvMembros = new System.Windows.Forms.DataGridView();
+            this.paginaDetalhes = new System.Windows.Forms.TabPage();
+            this.bnvMembros = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnNovoRegistro = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtNomeMembro = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtPapelMembro = new System.Windows.Forms.TextBox();
+            this.lblPapel = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.paginaDados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembros)).BeginInit();
+            this.paginaDetalhes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bnvMembros)).BeginInit();
+            this.bnvMembros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.paginaDados);
+            this.tabControl1.Controls.Add(this.paginaDetalhes);
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(489, 271);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // paginaDados
+            // 
+            this.paginaDados.Controls.Add(this.dgvMembros);
+            this.paginaDados.Location = new System.Drawing.Point(4, 22);
+            this.paginaDados.Name = "paginaDados";
+            this.paginaDados.Padding = new System.Windows.Forms.Padding(3);
+            this.paginaDados.Size = new System.Drawing.Size(481, 245);
+            this.paginaDados.TabIndex = 0;
+            this.paginaDados.Text = "Dados";
+            this.paginaDados.UseVisualStyleBackColor = true;
+            // 
+            // dgvMembros
+            // 
+            this.dgvMembros.AllowUserToAddRows = false;
+            this.dgvMembros.AllowUserToDeleteRows = false;
+            this.dgvMembros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMembros.Location = new System.Drawing.Point(0, 0);
+            this.dgvMembros.Name = "dgvMembros";
+            this.dgvMembros.Size = new System.Drawing.Size(481, 249);
+            this.dgvMembros.TabIndex = 0;
+            // 
+            // paginaDetalhes
+            // 
+            this.paginaDetalhes.Controls.Add(this.txtPapelMembro);
+            this.paginaDetalhes.Controls.Add(this.lblPapel);
+            this.paginaDetalhes.Controls.Add(this.txtNomeMembro);
+            this.paginaDetalhes.Controls.Add(this.lblNome);
+            this.paginaDetalhes.Controls.Add(this.txtId);
+            this.paginaDetalhes.Controls.Add(this.lblId);
+            this.paginaDetalhes.Location = new System.Drawing.Point(4, 22);
+            this.paginaDetalhes.Name = "paginaDetalhes";
+            this.paginaDetalhes.Padding = new System.Windows.Forms.Padding(3);
+            this.paginaDetalhes.Size = new System.Drawing.Size(481, 245);
+            this.paginaDetalhes.TabIndex = 1;
+            this.paginaDetalhes.Text = "Detalhes";
+            this.paginaDetalhes.UseVisualStyleBackColor = true;
+            // 
+            // bnvMembros
+            // 
+            this.bnvMembros.AddNewItem = null;
+            this.bnvMembros.CountItem = this.bindingNavigatorCountItem;
+            this.bnvMembros.DeleteItem = null;
+            this.bnvMembros.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.btnNovoRegistro,
+            this.bindingNavigatorDeleteItem});
+            this.bnvMembros.Location = new System.Drawing.Point(0, 0);
+            this.bnvMembros.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bnvMembros.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bnvMembros.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bnvMembros.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bnvMembros.Name = "bnvMembros";
+            this.bnvMembros.PositionItem = this.bindingNavigatorPositionItem;
+            this.bnvMembros.Size = new System.Drawing.Size(800, 25);
+            this.bnvMembros.TabIndex = 1;
+            this.bnvMembros.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(37, 22);
+            this.bindingNavigatorCountItem.Text = "de {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Número total de itens";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Mover primeiro";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Mover anterior";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Posição";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Posição atual";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Mover próximo";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "Mover último";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnNovoRegistro
+            // 
+            this.btnNovoRegistro.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNovoRegistro.Image = ((System.Drawing.Image)(resources.GetObject("btnNovoRegistro.Image")));
+            this.btnNovoRegistro.Name = "btnNovoRegistro";
+            this.btnNovoRegistro.RightToLeftAutoMirrorImage = true;
+            this.btnNovoRegistro.Size = new System.Drawing.Size(23, 22);
+            this.btnNovoRegistro.Text = "Adicionar novo";
+            this.btnNovoRegistro.Click += new System.EventHandler(this.btnNovoRegistro_Click);
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Excluir";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(94, 21);
+            this.txtId.MaxLength = 1000;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(173, 20);
+            this.txtId.TabIndex = 13;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(69, 24);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(18, 13);
+            this.lblId.TabIndex = 12;
+            this.lblId.Text = "ID";
+            // 
+            // txtNomeMembro
+            // 
+            this.txtNomeMembro.Enabled = false;
+            this.txtNomeMembro.Location = new System.Drawing.Point(94, 47);
+            this.txtNomeMembro.MaxLength = 20;
+            this.txtNomeMembro.Name = "txtNomeMembro";
+            this.txtNomeMembro.Size = new System.Drawing.Size(173, 20);
+            this.txtNomeMembro.TabIndex = 15;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(53, 50);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 14;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtPapelMembro
+            // 
+            this.txtPapelMembro.Enabled = false;
+            this.txtPapelMembro.Location = new System.Drawing.Point(94, 73);
+            this.txtPapelMembro.MaxLength = 10;
+            this.txtPapelMembro.Name = "txtPapelMembro";
+            this.txtPapelMembro.Size = new System.Drawing.Size(173, 20);
+            this.txtPapelMembro.TabIndex = 17;
+            // 
+            // lblPapel
+            // 
+            this.lblPapel.AutoSize = true;
+            this.lblPapel.Location = new System.Drawing.Point(53, 76);
+            this.lblPapel.Name = "lblPapel";
+            this.lblPapel.Size = new System.Drawing.Size(34, 13);
+            this.lblPapel.TabIndex = 16;
+            this.lblPapel.Text = "Papel";
+            // 
+            // frmMembro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bnvMembros);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmMembro";
+            this.Text = "Cadastrar - Membro Familiar";
+            this.Load += new System.EventHandler(this.frmMembro_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.paginaDados.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembros)).EndInit();
+            this.paginaDetalhes.ResumeLayout(false);
+            this.paginaDetalhes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bnvMembros)).EndInit();
+            this.bnvMembros.ResumeLayout(false);
+            this.bnvMembros.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage paginaDados;
+        private System.Windows.Forms.DataGridView dgvMembros;
+        private System.Windows.Forms.TabPage paginaDetalhes;
+        private System.Windows.Forms.BindingNavigator bnvMembros;
+        private System.Windows.Forms.ToolStripButton btnNovoRegistro;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.TextBox txtPapelMembro;
+        private System.Windows.Forms.Label lblPapel;
+        private System.Windows.Forms.TextBox txtNomeMembro;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+    }
+}
---
+++ pDesp/Views/frmMembro.cs
@@ -0,0 +1,57 @@
+﻿using pDesp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pDesp.Views
+{
+    public partial class frmMembro : Form
+    {
+        private readonly SqlConnection conexao;
+        private readonly DataSet dsMembro = new DataSet();
+        private readonly BindingSource bnMembro = new BindingSource();
+
+        public frmMembro(SqlConnection conexaoBd)
+        {
+            InitializeComponent();
+            conexao = conexaoBd;
+        }
+
+        private void frmMembro_Load(object sender, EventArgs e)
+        {
+            dsMembro.Tables.Add(Membro.Listar(conexao));
+            bnMembro.DataSource = dsMembro.Tables["MEMBRO"];
+            dgvMembros.DataSource = bnMembro;
+            bnvMembros.BindingSource = bnMembro;
+
+            txtId.DataBindings.Add(nameof(txtId.Text), bnMembro, "ID_MEMBRO");
+            txtNomeMembro.DataBindings.Add(nameof(txtNomeMembro.Text), bnMembro, "NOME_MEMBRO");
+            txtPapelMembro.DataBindings.Add(nameof(txtPapelMembro.Text), bnMembro, "PAPEL_MEMBRO");
+        }
+
+        private void btnNovoRegistro_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                tabControl1.SelectTab(1);
+            }
+            bnMembro.AddNew();
+            txtNomeMembro.Enabled = true;
+            txtPapelMembro.Enabled = true;
+            txtNomeMembro.Focus();
+            //btnSalvar.Enabled = true;
+            //btnAlterar.Enabled = false;
+            //btnNovoRegistro.Enabled = false;
+            //btnExcluir.Enabled = false;
+            //btnCancelar.Enabled = true;
+            //bInclusao = true;
+        }
+    }
+}
